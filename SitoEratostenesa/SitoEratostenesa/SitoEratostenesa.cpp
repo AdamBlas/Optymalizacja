1c4bba7775281a2bed0e38492bcf3d525b35fc61
Adam <adamblas97@gmail.com>
2021-10-10 19:02:46 +0200

    bugfix

--- SitoEratostenesa/SitoEratostenesa/SitoEratostenesa.cpp
+++ SitoEratostenesa/SitoEratostenesa/SitoEratostenesa.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 const int SASIN = 70000000;
 
-void eratos(char isPrime[], int index, int byte)
+void SieveOfEratosthenes(char isPrime[], int index, int byte)
 {
     if ((isPrime[index] >> byte) & 1)
     {
@@ -26,27 +26,41 @@ int main()
 {
     auto start_time = std::chrono::system_clock::now();
 
-    int size = ceil((float)SASIN / 8);
+    // Size of array with bit-size bool values
+    unsigned int size = ceil((float)SASIN / 8);
+
+    // Array of 8 bit chars, 8 bools per char
     char *isPrime = new char[size];
 
+    // Fill array with 1s
     for (int i = 0; i < size; i++)
     {
         isPrime[i] = 0xff;
     }
 
-    for (int i = 2; i < size; i++)
+    // Use Sieve of Eratosthenes on every number in array
+
+    // Skip two first bits (numbers 0 and 1 as there are not count to prime numbers)
+    for (int i = 2; i < 8; i++)
+    {
+        SieveOfEratosthenes(isPrime, 0, i);
+    }
+
+    // Go through all other indexes
+    for (int i = 1; i < size; i++)
     {
         for (int j = 0; j < 8; j++)
         {
-            eratos(isPrime, i, j);
+            SieveOfEratosthenes(isPrime, i, j);
         }
     }
 
-    long long sum = 0;
+    // Calculate sum, start from -1 to subtract 1, as it would be counted as prime number
+    long long sum = -1;
     for (int i = 0; i < size; i++)
     {
-        float i8 = i * 8;
-        for (int j = 0; j < 8; j++)
+        int i8 = i * 8;
+        for (short j = 0; j < 8; j++)
         {
             if ((isPrime[i] >> j) & 1)
             {
@@ -59,10 +73,11 @@ int main()
     cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(processing_time).count() << endl;
     cout << "Sum: " << sum << endl;
 
-    if (sum != 139601928199359)
+    long long correctSum = 139601928199359;
+    if (sum != correctSum)
     {
         cout << "ERROR" << endl;
-        cout << "DIFF: " << sum - 139601928199359 << endl;
+        cout << "DIFF: " << sum - correctSum << endl;
     }
 
     return 0;
